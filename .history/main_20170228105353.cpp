e24403d061bbd9b738bc0d8ac25e79d9a7a7de0c
kiskompi <kiskompi@gmail.com>
2017-03-12 16:50:33 +0100

    structure creation working, folder creaton doesnt

---
+++ .history/main_20170228105353.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <boost/program_options.hpp>
+
+#include "dirbuilder/dirbuilder.hpp"
+
+namespace po = boost::program_options;
+
+int main(int argc, char** argv)
+{
+    std::string path;
+    std::string file_name;
+    bool cmake;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+    ("help", "produce help message")
+    ("path,P", po::value<std::string>(&path), "Set project path")
+    ("cmake,C", po::value<bool>(&cmake) -> default_value(true), "Creates cMake.txt if true")
+    ("file,F", po::value<std::string>(&path), "Set file to read, must be in path directory! If not set, default structure will be built.")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+    DirBuilder db;
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+
+    if (vm.count("file"))
+        db = DirBuilder(vm["file"].as<std::string>());
+    else
+        std::cout << "File was not set.\n";
+
+    if (vm.count("path"))
+        db.create_dirs_with_path(path);
+    else
+        std::cout << "Path was not set.\n";
+
+    if (vm.count("cmake"))
+        std::cout << "cMake is:" << vm["cmake"].as<bool>() << std::endl;
+    else
+        std::cout << "cMake was not set.\n";
+    db.print_structure();
+}
--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 This app will be a simple project structure generator. It is under development and in alpha state. Right now it only works as a folder structure generator, but later more features will be added. Created only to practice the language by making something potentially useful thing.
 
 ## Features to implement
-[x] Folder structure generation
-[ ] Extend the previous with permission handling.
-[ ] Project and Folder generation modes
-[ ] Makefile generation for standard project buildups
+- [x] Folder structure generation
+- [ ] Extend the previous with permission handling.
+- [ ] Project and Folder generation modes
+- [ ] Makefile generation for standard project buildups
 
 ## How to use it
 Build the source with build.sh. The following flags are to use:
@@ -25,4 +25,8 @@ name1#333
 |--name4
 name7
 
-Where the folders tabbed with more '|--' strings than the previous line are the children of that, and #xxx are the permissions on the folder in chmod form.
+Where the folders tabbed with more '|--' strings than the previous line are the children of that, and #xxx are the permissions on the folder in chmod form:
+> 4 stands for "read",
+> 2 stands for "write",
+> 1 stands for "execute", and
+> 0 stands for "no permission."
